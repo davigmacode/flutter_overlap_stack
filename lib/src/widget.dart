c3739e7bd401058385400d49c2d807f08802e828
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-03-26 14:38:49 +0700

    Added ability to create stack from explicit list, and other adjustment

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -2,76 +2,110 @@ import 'dart:math';
 import 'package:flutter/widgets.dart';
 import 'types.dart';
 
+/// Intelligently stacks widgets,
+/// allowing them to partially overlap
+/// for a compact and visually interesting layout.
 class OverlapStack extends StatelessWidget {
-  const OverlapStack({
+  /// Creates an overlapping stack, linear array of widgets from an explicit [List].
+  OverlapStack({
     super.key,
     this.direction = Axis.horizontal,
     this.align = OverlapStackAlign.center,
-    this.minCoverage = .5,
-    this.maxCoverage = .9,
+    this.minSpacing = .5,
+    this.maxSpacing = .8,
     this.leadIndex = -1,
     this.infoIndex = -1,
     this.infoBuilder,
     this.itemLimit = -1,
-    required this.itemSize,
-    required this.itemLength,
-    required this.itemBuilder,
-  })  : assert(minCoverage > 0 && maxCoverage >= minCoverage),
-        assert(itemLength >= 0),
-        assert(itemLimit <= itemLength);
+    this.itemSize,
+    required List<Widget> children,
+  })  : assert(minSpacing > 0 && maxSpacing >= minSpacing),
+        assert(leadIndex >= -1),
+        assert(infoIndex >= -1),
+        assert(itemLimit >= -1),
+        itemCount = children.length,
+        itemBuilder = ((context, index) => children[index]);
 
+  /// Creates an overlapping stack, linear array of widgets that are created on demand.
+  const OverlapStack.builder({
+    super.key,
+    this.direction = Axis.horizontal,
+    this.align = OverlapStackAlign.center,
+    this.minSpacing = .5,
+    this.maxSpacing = .8,
+    this.leadIndex = -1,
+    this.infoIndex = -1,
+    this.infoBuilder,
+    this.itemLimit = -1,
+    this.itemSize,
+    required this.itemCount,
+    required this.itemBuilder,
+  })  : assert(minSpacing > 0 && maxSpacing >= minSpacing),
+        assert(leadIndex >= -1),
+        assert(infoIndex >= -1),
+        assert(itemLimit >= -1),
+        assert(itemCount >= 0),
+        assert(itemLimit <= itemCount);
+
+  /// The direction to use as the main axis.
   final Axis direction;
+
+  /// How the children should be placed along the main axis.
   final OverlapStackAlign align;
-  final double minCoverage;
-  final double maxCoverage;
+
+  /// Defines minimum spacing between items,
+  /// as a percentage of item extent.
+  final double minSpacing;
+
+  /// Defines maximum spacing between items,
+  /// as a percentage of item extent.
+  final double maxSpacing;
+
+  /// Controls which item appears at the top.
   final int leadIndex;
+
+  /// Controls the position of the info widget (if present).
   final int infoIndex;
-  final int itemLimit;
-  final Size itemSize;
-  final int itemLength;
-  final IndexedWidgetBuilder itemBuilder;
+
+  /// Callback for providing details of invisible children.
   final OverlapStackInfoBuilder? infoBuilder;
+
+  /// Number of items to display.
+  final int itemLimit;
+
+  /// Defines the size (width or height) of an individual item in the stack.
+  final Size? itemSize;
+
+  /// The total number of children this delegate can provide.
+  final int itemCount;
+
+  /// Called to build children
+  final IndexedWidgetBuilder itemBuilder;
 
   bool get isHorizontal => direction == Axis.horizontal;
   bool get isVertical => !isHorizontal;
 
-  List<Widget> _itemLayering(List<Widget> items, int lastIndex) {
-    // last on top
-    if (leadIndex == -1 || leadIndex >= lastIndex) return items;
-
-    // first on top
-    if (leadIndex == 0) return items.reversed.toList();
-
-    // middle on top
-    final leftSide = items.getRange(0, leadIndex);
-    final rightSide = items.getRange(leadIndex, lastIndex);
-    return [
-      ...leftSide,
-      ...rightSide.toList().reversed,
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: isVertical ? itemSize.width : null,
-      height: isHorizontal ? itemSize.height : null,
+      width: isVertical ? itemSize?.width : null,
+      height: isHorizontal ? itemSize?.height : null,
       child: LayoutBuilder(
         builder: (context, constraints) {
-          final double itemExtent =
-              isHorizontal ? itemSize.width : itemSize.height;
-          final double itemExtentMax = itemExtent * maxCoverage;
-          final double itemExtentMin = itemExtent * minCoverage;
+          final double itemExtent = isHorizontal
+              ? (itemSize?.width ?? constraints.maxHeight)
+              : (itemSize?.height ?? constraints.maxWidth);
+          final double itemExtentMax = itemExtent * maxSpacing;
+          final double itemExtentMin = itemExtent * minSpacing;
           final double itemTrailExtent = itemExtent - itemExtentMin;
 
           final double maxExtent =
               isHorizontal ? constraints.maxWidth : constraints.maxHeight;
           final double allowedExtent = maxExtent - itemTrailExtent;
           final int itemAllowed = allowedExtent ~/ itemExtentMin;
-          final int itemMax = itemLimit < 0 ? itemLength : itemLimit;
-          final int itemDisplay =
-              [itemAllowed, itemLength, itemMax].reduce(min);
-          final int itemRemaining = itemLength - itemDisplay;
+          final int itemMax = itemLimit < 0 ? itemCount : itemLimit;
+          final int itemDisplay = [itemAllowed, itemCount, itemMax].reduce(min);
+          final int itemRemaining = itemCount - itemDisplay;
 
           final double takenSpace = itemDisplay * itemExtentMin;
           final double freeSpace = allowedExtent - takenSpace;
@@ -106,10 +140,25 @@ class OverlapStack extends StatelessWidget {
             ),
             growable: false,
           );
-
-          return Stack(children: _itemLayering(children, itemDisplay));
+          return Stack(children: _reordering(children, itemDisplay));
         },
       ),
     );
+  }
+
+  List<Widget> _reordering(List<Widget> items, int lastIndex) {
+    // last on top
+    if (leadIndex < 0 || leadIndex >= lastIndex) return items;
+
+    // first on top
+    if (leadIndex == 0) return items.reversed.toList();
+
+    // middle on top
+    final leftSide = items.getRange(0, leadIndex);
+    final rightSide = items.getRange(leadIndex, lastIndex);
+    return [
+      ...leftSide,
+      ...rightSide.toList().reversed,
+    ];
   }
 }
